dacf1bb4dbe93b4a0f0b702885a21771dd2a1a1c
Ali Hamza <90191882+DevHumzaah@users.noreply.github.com>
2024-08-04 20:15:47 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"  //treat all JS code as new version
+
+
+// alert(4+4) we are using node js not browser
+
+
+console.log(3 
+    + 3) ;              //code readability should high
+
+console.log("I am Hamza");
+
+ 
+//DATATYPES
+
+let name = "ALI"  //string
+let age =24       // number ==> 2 power 53  ==>bigint
+let ispass = true  //boolean
+let state;  //undefined -->type is undefined
+let gender= null  // null -->type is object
+
+// objects, symbols
+
+console.log(typeof age);
+
+
