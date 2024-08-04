fbe7938ebacfedf683aa8d5401cba59f304fdd0e
Ali Hamza <90191882+DevHumzaah@users.noreply.github.com>
2024-08-04 19:54:14 +0000

    variable done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId= 123457
+var email= "ali@gmail.com"
+let password= "092287"
+
+// accountId= 16267   this means in const the value can't be changed
+
+// var can not be used because of block scope and functional scope now only let and const are used 
+
+email= "hamza@gmail.com"
+password="0011"
+
+console.log(email);
+console.log(password);
+
+
+console.log(accountId);
