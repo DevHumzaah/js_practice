1ff72c20247f8dbf7f40769105c825c132a5a4a5
Ali Hamza <90191882+DevHumzaah@users.noreply.github.com>
2024-08-05 21:23:54 +0000

    datatypes

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+
+//primitive
+
+// 7 types => string, number, boolean, null, undefined, symbol, bigint
+
+
+//non-primitive
+
+// array, objects, functions
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
