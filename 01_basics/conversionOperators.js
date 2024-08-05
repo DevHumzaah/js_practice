d1be04feadfa7dafd63b3cd28b35a93723d5f276
Ali Hamza <90191882+DevHumzaah@users.noreply.github.com>
2024-08-05 20:58:03 +0000

    operators

---
+++ 01_basics/conversionOperators.js
@@ -0,0 +1,56 @@
+let score= 33
+let score2= "33"
+
+
+console.log(typeof score)
+console.log(typeof(score2));
+
+
+// "33" ==> it can be simply be convert
+//"33abs" ==> it cant be convert and give Nan Value
+
+//true=1 , false=0
+
+
+let islogin= 1
+
+let booleanIsLogin = Boolean(islogin)
+
+console.log(booleanIsLogin);
+
+
+//when here we use 1 we get true and "" if it is not given any value we get false
+//"ali" true dy ga value ager string ma value de jaye to
+
+
+
+
+           //operations
+
+
+     let value = 3
+     
+     let negValue= -value
+
+     console.log(negValue);
+     
+    //  console.log(2+2);
+    //  console.log(2-2);
+    //  console.log(2*2);
+    //  console.log(2**4);
+    //  console.log(2/2);
+
+
+    let str1 = "ali"
+    let str2 = "hamza"
+
+    console.log(str1+str2);
+
+
+    console.log(1 + "2");
+    console.log("1" + 2);
+    console.log(1 + 3 + "2"); // * important
+    console.log(1 + "2" +4);
+    
+    
+     
