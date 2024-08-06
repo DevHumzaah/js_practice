174f88f8ad96ba3db4a9ddaca765f84e50ab74c9
Ali Hamza <90191882+DevHumzaah@users.noreply.github.com>
2024-08-06 16:51:33 +0000

    Destructring of Object

---
+++ 02_basics/objects.js
@@ -0,0 +1,16 @@
+const course ={
+    courseName :"English",
+    courseInstructor :"ALI HAMZA",
+    coursePrice : "1000"
+
+}
+
+const {courseInstructor}= course
+console.log(courseInstructor, );   //destructring
+
+// {
+//     name: "Ali",
+//     course: "English",
+
+// }
+
